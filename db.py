dd85b1cbc213cc6dd0e93e338608036a2bedcd9b
rodriguesJonathan <jonathanlr2000@gmail.com>
2022-04-15 15:53:48 -0300

    project finish

--- db.py
+++ db.py
@@ -1,13 +1,63 @@
 def inserirNoBanco(*args):
 	file = open("db.txt", 'a')
-	file.write("%s;%s;%s;%s\n"%(args))
+	file.write(("%s;%s;%s;%s\n"%(args)).lower())
 	file.close()
 
 def lerTodoBancoDeDados():
 	lista = []
-	file = open("db.txt", 'r')
-	linhas = file.readlines()
-	file.close()
+	arquivo = open("db.txt", 'r')
+	linhas = arquivo.readlines()
+	arquivo.close()
+	num = 1
 	for linha in linhas:
 		lista.append(linha.strip().split(";"))
+		num += 1
+	return lista
+
+def buscarPorNomeArtistaBanda(nome):
+	nome = nome.lower()
+	lista = []
+	arquivo = open("db.txt", 'r')
+	linhas = arquivo.readlines()
+	arquivo.close()
+	num = 1
+	for linha in linhas:
+		linha = linha.strip().split(";")
+		if nome in linha[2]:
+			lista.append([num] + linha)
+			num += 1
+	return lista
+
+def buscarPorAnoBanda(anoEscolhido, condicao):
+	ANTERIOR = 1
+	IGUAL = 2
+	POSTERIOR = 3
+
+	lista = []
+	arquivo = open("db.txt", 'r')
+	linhas = arquivo.readlines()
+	arquivo.close()
+
+	num = 1
+	if condicao == ANTERIOR:
+		for linha in linhas:
+			linha = linha.strip().split(";")
+			anoAlbum = int(linha[1])
+			if anoAlbum <= anoEscolhido:
+				lista.append([num]+linha)
+				num+=1
+	elif condicao == IGUAL:
+		for linha in linhas:
+			linha = linha.strip().split(";")
+			anoAlbum = int(linha[1])
+			if anoAlbum == anoEscolhido:
+				lista.append([num]+linha)
+				num+=1
+	elif condicao == POSTERIOR:
+		for linha in linhas:
+			linha = linha.strip().split(";")
+			anoAlbum = int(linha[1])
+			if anoAlbum >= anoEscolhido:
+				lista.append([num]+linha)
+				num+=1
 	return lista
--- domain.py
+++ domain.py
@@ -1,19 +1,101 @@
-from distutils.command.build import build
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.recycleview import RecycleView
+from kivy.uix.dropdown import DropDown
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.lang.builder import Builder
+
 import db
 
+stringHeader = """
+<CustomDropDown>:
+"""
 
-class ListaScreen(BoxLayout):
-	def on_press_voltar(self):
-		window.root_window.remove_widget(window.root)
-		window.root_window.add_widget(FirstScreen())
 
-class RV(RecycleView):
+model = """
+	Button:
+		text: '{0}'
+		size_hint_y: None
+		height: 44
+		on_release: root.select('{0}')
+"""
+for ano in range(1990,2023):
+    stringHeader += model.format(ano)
+
+stringBuilder = Builder.load_string(stringHeader)
+
+class CustomDropDown(DropDown):
+    def build():
+        return stringBuilder
+
+
+class DropdownDemo(FloatLayout):
+	anoEscolhido = 0
+	def __init__(self, **kwargs):
+		super(DropdownDemo, self).__init__(**kwargs)
+		self.dropdown = CustomDropDown()
+		self.mainbutton = Button(text ='Escolha o Ano',
+								size_hint = (1., 1.),
+								pos_hint = {"top":1., "center_x":.5})
+		self.add_widget(self.mainbutton)
+		self.mainbutton.bind(on_release = self.dropdown.open)
+		self.dropdown.bind(on_select = lambda\
+						instance, x: setattr(self.mainbutton, 'text', x))
+		self.dropdown.bind(on_select = self.callback)
+
+	def callback(self, instance, ano):
+		self.anoEscolhido = int(ano)
+
+class RVAnoScreen(RecycleView):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		print(kwargs)
+		self.data = []
+
+class BuscaPorAnoScreen(BoxLayout):
+	checkBoxSelect = 0
+
+	def showDataAno(self):
+		lista = []
+		model = "Numero:\n%d\nNome do Album:\n%s\nAno do lancamento:\n%s\nBanda/Artista:\n%s\nAlbum lancamento do artista:\n%s\n"
+		listaNomes = db.buscarPorAnoBanda(self.ids.dropdown.anoEscolhido, self.checkBoxSelect)
+
+		for linha in listaNomes:
+			num, nomeAlbum, anoLancamento, bandaArtista, lancamentoAB = linha
+			lista.append( model%(num,nomeAlbum, anoLancamento, bandaArtista, lancamentoAB))
+		self.ids.recycleBuscaAno.data = [{'text': text} for text in lista]
+
+	def checkBoxClick(self, instace, value, condicao):
+		if value == True:
+			self.checkBoxSelect = condicao
+			print(f"{condicao} ativo")
+	
+class RVNomeScreen(RecycleView):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.data = []
+
+class BuscaPorNomeScreen(BoxLayout):
+	def showData(self):
+		nome = self.ids.nomeParaBusca.text
+		lista = []
+		model = "Numero:\n%d\nNome do Album:\n%s\nAno do lancamento:\n%s\nBanda/Artista:\n%s\nAlbum lancamento do artista:\n%s\n"
+		listaNomes = db.buscarPorNomeArtistaBanda(nome)
+
+
+		for linha in listaNomes:
+			num, nomeAlbum, anoLancamento, bandaArtista, lancamentoAB = linha
+			lista.append( model%(num,nomeAlbum, anoLancamento, bandaArtista, lancamentoAB))
+		
+		
+		self.ids.recycleBuscaNome.data = [{'text': text} for text in lista]
+
+class ListaScreen(BoxLayout):
+	pass
+
+class RVListaScreen(RecycleView):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
 		modelList = []
 		model = "Numero:\n%d\nNome do Album:\n%s\nAno do lancamento:\n%s\nBanda/Artista:\n%s\nAlbum lancamento do artista:\n%s\n"
 		num = 1
@@ -25,9 +107,6 @@ class RV(RecycleView):
 		
 		self.data = [{'text': text} for text in modelList]
 
-
-
-
 class FirstScreen(BoxLayout):
 	checkBoxSelect = "Dado não informado"
 
@@ -36,7 +115,7 @@ class FirstScreen(BoxLayout):
 			self.checkBoxSelect = f'{text}'
 			print(f"{text} ativo")
 
-	def on_press_salvar(self):
+	def onPressSalvar(self):
 		nomeAlbum     = self.ids.nomeAlbum.text
 		anoLancamento = self.ids.anoLancamento.text
 		bandaArtista  = self.ids.bandaArtista.text
@@ -46,13 +125,24 @@ class FirstScreen(BoxLayout):
 
 		self.ids.nomeAlbum.text, self.ids.anoLancamento.text, self.ids.bandaArtista.text, self.ids.lancamentoAB.text = "","","",""
 
-	def on_press_lista(self):
+	def onPressLista(self):
 		window.root_window.remove_widget(window.root)
 		window.root_window.add_widget(ListaScreen())
+	
+	def onPressBuscaNome(self):
+		window.root_window.remove_widget(window.root)
+		window.root_window.add_widget(BuscaPorNomeScreen())
+	
+	def onPressBuscaAno(self):
+		window.root_window.remove_widget(window.root)
+		window.root_window.add_widget(BuscaPorAnoScreen())
+
 
 
 class GUIApp(App):
-	pass
+	def onPressVoltar(self):
+		window.root_window.remove_widget(window.root)
+		window.root_window.add_widget(FirstScreen())
 
 if __name__ == "__main__":
 	window = GUIApp()
